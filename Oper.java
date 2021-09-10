4b55d7d23cbd427830166cf3bfb8c0bc95fc028e
Nataly Kash <87307949+Doskonala@users.noreply.github.com>
2021-09-10 17:15:43 +0300

    Add files via upload

---
+++ Oper.java
@@ -0,0 +1,23 @@
+
+public class Oper {
+	private int a;
+	private int b;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	int sum() {
+		return a+b;
+	}
+	int dif() {
+		return a-b;
+	}
+}
---
+++ Probal.java
@@ -0,0 +1,11 @@
+
+public class Probal {
+
+	public static void main(String[] args) {
+		Oper op = new Oper();
+		op.setA(6);
+		op.setB(7);
+		System.out.println("Сума="+op.sum());
+		System.out.println("Різниця="+op.dif());
+	}
+}
